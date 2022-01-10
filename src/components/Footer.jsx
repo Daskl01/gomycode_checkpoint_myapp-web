5948259fb1c81c3b3fa51bd61bff831aaca68573
Daskl01 <aboubackrskl@gmail.com>
2022-01-10 22:11:38 +0000

    End projects

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const footer = () => {
+    return (
+        <div className="footer bg-primary text-center py-3 text-white   ">
+            <h1 className="h6">Designed By @ Daskl</h1>
+        </div>
+    )
+}
+
+export default footer;
